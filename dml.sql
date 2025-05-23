a5ce76c31faf1ee2949d8fc4f5ae00e7f157c6c9
Ashby <ashby.loshin@gmail.com>
2025-05-23 00:38:01 -0400

    Update dml.sql
    Update dml.sql to include JOIN in SELECT queries for FantasyTeams, GamePlayed, and RosteredPlayers

--- dml.sql
+++ dml.sql
@@ -13,7 +13,10 @@ UPDATE FantasyLeagues SET name = :name WHERE leagueID = :leagueID;
 DELETE FROM FantasyLeagues WHERE leagueID = :leagueID;
 
 -- FantasyTeams
-SELECT teamID, name, wins, losses, leagueID, managerID FROM FantasyTeams;
+SELECT t.teamID, t.name AS name, t.wins, t.losses, t.leagueID, l.name AS leagueName, t.managerID, m.name AS managerName
+  FROM FantasyTeams t
+  JOIN FantasyLeagues l ON t.leagueID = l.leagueID
+  JOIN FantasyManagers m ON t.managerID = m.managerID;
 INSERT INTO FantasyTeams (name, wins, losses, leagueID, managerID) VALUES (:name, :wins, :losses, :leagueID, :managerID);
 UPDATE FantasyTeams SET wins = :wins, losses = :losses WHERE teamID = :teamID;
 DELETE FROM FantasyTeams WHERE teamID = :teamID;
@@ -32,7 +35,10 @@ UPDATE NBAGames SET date = :date WHERE gameID = :gameID;
 DELETE FROM NBAGames WHERE gameID = :gameID;
 
 -- GamePlayed
-SELECT id, gameID, playerID, points, threes, assists, blocks, steals, fgm, fga, ftm, fta, rebounds, turnovers FROM GamePlayed;
+SELECT gp.id, gp.gameID, p.name AS playerName, gp.points, gp.threes, gp.assists, gp.blocks,
+    gp.steals, gp.fgm, gp.fga, gp.ftm, gp.fta, gp.rebounds, gp.turnovers
+  FROM GamePlayed gp
+  JOIN NBAPlayers p ON gp.playerID = p.playerID;
 INSERT INTO GamePlayed (gameID, playerID, points, threes, assists, blocks, steals, fgm, fga, ftm, fta, rebounds, turnovers)
 VALUES (:gameID, :playerID, :points, :threes, :assists, :blocks, :steals, :fgm, :fga, :ftm, :fta, :rebounds, :turnovers);
 UPDATE GamePlayed SET points=:points, threes=:threes WHERE id=:id;
@@ -45,7 +51,10 @@ UPDATE WaiverTransactions SET transactionType = :transactionType WHERE transacti
 DELETE FROM WaiverTransactions WHERE transactionID = :transactionID;
 
 -- RosteredPlayers
-SELECT id, teamID, playerID, dateAdded, isStarter FROM RosteredPlayers;
+SELECT r.id, r.teamID, r.playerID, r.dateAdded, r.isStarter, p.name AS playerName, t.name AS teamName
+  FROM RosteredPlayers r
+  JOIN NBAPlayers p ON r.playerID = p.playerID
+  JOIN FantasyTeams t ON r.teamID = t.teamID;
 INSERT INTO RosteredPlayers (teamID, playerID, dateAdded, isStarter) VALUES (:teamID, :playerID, :dateAdded, :isStarter);
 UPDATE RosteredPlayers SET isStarter = :isStarter WHERE id = :id;
 DELETE FROM RosteredPlayers WHERE id = :id;
